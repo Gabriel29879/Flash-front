5d82ab9236d7e6a67a75a163dfb00e91d9934147
Gabriel <gabriel29879@gmail.com>
2021-02-03 02:35:07 -0300

    Feature: Tabela implementada Descrição: Base da tabela com dados falsos implementada, projeto quase pronto para a integração com a API

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Button from '../components/Button'
 
 import 'antd/dist/antd.css';
-import { Select } from 'antd';
+import { Table,Select } from 'antd';
 const { Option } = Select;
 
 const companyArr = [
@@ -12,6 +12,32 @@ const companyArr = [
     {id: 3, name: "Razer", CNPJ: "14.390.714/0001-68", endereco: { cidade: "Curitiba", estado: "Parana", rua: "Av teste", numero: "365", CEP: "87641-230" }, beneficios: ["VA", "VT"]},
     {id: 4, name: "Intel", CNPJ: "11.222.333/0001-68", endereco: { cidade: "Curiasefase", estado: "aesfase", rua: "Av dhte", numero: "222", CEP: "22222-222" }, beneficios: ["VA", "VT"]}
 ];
+
+const funcionarios = [
+    { name: "Lucas Martir", CPF: "222.333.444-87", email: "teste@teste.com" },
+    { name: "Lucas Martir", CPF: "222.333.444-87", email: "teste@teste.com" },
+    { name: "Lucas Martir", CPF: "222.333.444-87", email: "teste@teste.com" },
+    { name: "Lucas Martir", CPF: "222.333.444-87", email: "teste@teste.com" },
+    { name: "Lucas Martir", CPF: "222.333.444-87", email: "teste@teste.com" },
+]
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'CPF',
+        dataIndex: 'CPF',
+        key: 'CPF',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email',
+    }
+]
 
 const App = () => {
     const [conpany, setConpany] = useState();
@@ -45,28 +71,27 @@ const App = () => {
     return (
         <>
         <div className="main-header">Flash</div>
-        <div className="container">
-            <div className="card">
-                <p>Caso tenha interesse, você pode cadastrar sua empresa em nosso sistema ou incluir um novo funcionário em alguma das empresas já existentes.</p>
-                <div className="upper-card-btns">
-                    <Button text="ADICIONAR EMPRESA" />
-                    <Button text="ADICIONAR FUNCIONARIO" />
-                </div>
+        <div className="card">
+            <p>Caso tenha interesse, você pode cadastrar sua empresa em nosso sistema ou incluir um novo funcionário em alguma das empresas já existentes.</p>
+            <div className="upper-card-btns">
+                <Button text="ADICIONAR EMPRESA" />
+                <Button text="ADICIONAR FUNCIONARIO" />
             </div>
-            <div className="card middle-card">
-                <h2>Selecione uma empresa no campo abaixo para ver sua lista de funcionários.</h2>
-                <Select 
-                    placeholder={conpany ? "Selecione uma empresa" : "Voce deve criar pelo menos uma empresa"} 
-                    style={{ width: 500 }} 
-                    onChange={selectChange}>
-                    {
-                        companyArr.map(item => <Option value={item.id} key={item.id}>{item.name}</Option>)
-                    }
-                </Select>
-            </div>
-            <div className="card table">
-                    {conpany ? renderTableHeader(conpany[0]) : ""}
-            </div>
+        </div>
+        <div className="card middle-card">
+            <h2>Selecione uma empresa no campo abaixo para ver sua lista de funcionários.</h2>
+            <Select 
+                placeholder={conpany ? "Selecione uma empresa" : "Voce deve criar pelo menos uma empresa"} 
+                style={{ width: 500 }} 
+                onChange={selectChange}>
+                {
+                    companyArr.map(item => <Option value={item.id} key={item.id}>{item.name}</Option>)
+                }
+            </Select>
+        </div>
+        <div className="card table">
+                {conpany ? renderTableHeader(conpany[0]) : ""}
+                <Table columns={columns} dataSource={funcionarios} />
         </div>
         </>
     );
