a57f85cf052ddf610409022ca9d630ae767c2b32
Gabriel <gabriel29879@gmail.com>
2021-02-03 01:43:07 -0300

    feature: Adicionado base do header da table Descrição: Header da table adicionado com dados placeholder, função de render para header da table implementada

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,17 +7,39 @@ import { Select } from 'antd';
 const { Option } = Select;
 
 const companyArr = [
-    {name: "NuBank"},
-    {name: "Google"},
-    {name: "Razer"},
-    {name: "Intel"}
+    {id: 1, name: "NuBank", CNPJ: "14.390.714/0001-68", endereco: { cidade: "Curitiba", estado: "Parana", rua: "Av teste", numero: "365", CEP: "87641-230" }, beneficios: ["VA", "VT"]},
+    {id: 2, name: "Google", CNPJ: "14.390.714/0001-68", endereco: { cidade: "Curitiba", estado: "Parana", rua: "Av teste", numero: "365", CEP: "87641-230" }, beneficios: ["VA", "VT"]},
+    {id: 3, name: "Razer", CNPJ: "14.390.714/0001-68", endereco: { cidade: "Curitiba", estado: "Parana", rua: "Av teste", numero: "365", CEP: "87641-230" }, beneficios: ["VA", "VT"]},
+    {id: 4, name: "Intel", CNPJ: "11.222.333/0001-68", endereco: { cidade: "Curiasefase", estado: "aesfase", rua: "Av dhte", numero: "222", CEP: "22222-222" }, beneficios: ["VA", "VT"]}
 ];
 
 const App = () => {
     const [conpany, setConpany] = useState();
 
     const selectChange = (value) => {
-        setConpany(value);
+        const result = companyArr.filter(item => { if(item.id === value){ return item } else { return null } });
+        setConpany(result);
+        console.log(conpany);
+    }
+
+    const renderTableHeader = (conpany) => {
+        if(conpany) {
+            return (
+                <>
+                    <h1>{conpany.name}</h1>
+                    <p>{"CNPJ: " + conpany.CNPJ}</p>
+                    <p>{
+                            "Endereço: " +
+                            conpany.endereco.rua + ", " + 
+                            conpany.endereco.numero + ", " +
+                            conpany.endereco.cidade + " - " + 
+                            conpany.endereco.estado + ", " + 
+                            conpany.endereco.CEP
+                        }</p>
+                    <p>{"Beneficios: " + conpany.beneficios}</p>
+                </>
+            );
+        }
     }
 
     return (
@@ -38,9 +60,12 @@ const App = () => {
                     style={{ width: 500 }} 
                     onChange={selectChange}>
                     {
-                        companyArr.map(item => <Option value={item.name} key={item.name}>{item.name}</Option>)
+                        companyArr.map(item => <Option value={item.id} key={item.id}>{item.name}</Option>)
                     }
                 </Select>
+            </div>
+            <div className="card table">
+                    {conpany ? renderTableHeader(conpany[0]) : ""}
             </div>
         </div>
         </>
